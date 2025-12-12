52a464cf72449b0d6585c8578f1fc656b4142324
rayyanrayyan529-art <rayyanrayyan529@gmail.com>
2025-12-12 16:58:45 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="UAS Kecerdasan Artifisial", layout="wide")
+
+st.title("UAS Kecerdasan Artifisial")
+st.write("Aplikasi berhasil dijalankan!")
+st.success("Selamat! Streamlit sudah berjalan dengan baik di komputer Anda.")
